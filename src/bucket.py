5e7f8f2bd73bb5ec7910f848f36ae68e7e55b29e
Zerka30 <contact.zerka@gmail.com>
2023-10-14 21:46:06 +0200

    🎉 (backup): Init project

---
+++ src/bucket.py
@@ -0,0 +1,73 @@
+import boto3
+import os
+import logging
+
+
+class S3Bucket:
+    def __init__(self, endpoint_url, bucket_name, access_key, secret_key):
+        self.endpoint_url = endpoint_url
+        self.bucket_name = bucket_name
+        self.access_key = access_key
+        self.secret_key = secret_key
+        self.s3_client = boto3.client(
+            "s3",
+            endpoint_url=endpoint_url,
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key,
+        )
+
+    def list_objects(self):
+        # List objects in the S3 bucket
+        objects = self.s3_client.list_objects_v2(Bucket=self.bucket_name)
+        return objects.get("Contents", [])
+
+    def upload_folder(
+        self, source_path, service_name, current_datetime, bakignore_rules
+    ):
+        # Create a repository for the backup (format: service_name/date_time)
+        backup_folder_name = (
+            f"{service_name}/{current_datetime.strftime('%Y-%m-%d_%H-%M-%S')}"
+        )
+
+        # Get the base path for the rules
+        base_path = os.path.abspath(source_path)
+
+        for root, dirs, files in os.walk(source_path):
+            for file in files:
+                source_file = os.path.join(root, file)
+                s3_key = os.path.join(
+                    backup_folder_name, os.path.relpath(source_file, source_path)
+                )
+
+                # Make the source_file path relative to the base_path
+                rel_source_file = os.path.relpath(source_file, base_path)
+                if not bakignore_rules(rel_source_file):
+                    # Copy file to S3 Bucket
+                    try:
+                        self.s3_client.upload_file(
+                            source_file,
+                            self.bucket_name,
+                            s3_key,
+                            ExtraArgs={"ACL": "private"},
+                            Callback=None,
+                        )
+                        logging.info(f"Backup: {s3_key} - Success")
+                    except Exception as e:
+                        logging.error(f"Error while backing up {s3_key}: {str(e)}")
+                else:
+                    logging.info(f"Backup: {s3_key} - File ignored")
+
+    def delete_folder(self, folder_name):
+        # List objects in the S3 bucket
+        objects = self.list_objects()
+        objects_to_delete = [
+            obj["Key"] for obj in objects if obj["Key"].startswith(folder_name)
+        ]
+
+        for obj_key in objects_to_delete:
+            try:
+                # Delete objects that match the folder_name
+                self.s3_client.delete_object(Bucket=self.bucket_name, Key=obj_key)
+                logging.info(f"Deleted object: {obj_key}")
+            except Exception as e:
+                logging.error(f"Error while deleting object {obj_key}: {str(e)}")
---
+++ src/main.py
@@ -0,0 +1,94 @@
+import yaml
+import logging
+from datetime import datetime, timedelta
+from gotify import Gotify
+from gitignore_parser import parse_gitignore
+
+from bucket import S3Bucket
+
+# Configure logging
+logging.basicConfig(
+    filename="backup.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
+if __name__ == "__main__":
+    # Load configuration from the config.yml file
+    with open("./config.yml", "r") as config_file:
+        config = yaml.load(config_file, Loader=yaml.FullLoader)
+
+    # Create an S3Bucket instance with configuration settings
+    bucket = S3Bucket(
+        config["s3"]["endpoint_url"],
+        config["s3"]["bucket_name"],
+        config["s3"]["access_key"],
+        config["s3"]["secret_key"],
+    )
+    current_datetime = datetime.now()
+
+    # Create a Gotify instance for sending notifications
+    gotify = Gotify(config["gotify"]["api_url"], config["gotify"]["api_token"])
+
+    # Initialize the summary message
+    summary = []
+
+    # Read the .bakignore file and parse the ignore rules
+    bakignore_rules = parse_gitignore(".bakignore")
+
+    # Start the backup process for each configured folder
+    for folder_info in config["backup_folders"]:
+        source_path = folder_info["path"]
+        service_name = folder_info["service_name"]
+
+        # Log that we are starting the backup for the service
+        logging.info(f"Starting backup for service: {service_name}")
+
+        try:
+            # Save the folder
+            bucket.upload_folder(
+                source_path, service_name, current_datetime, bakignore_rules
+            )
+            logging.info(f"Backup for service {service_name} completed successfully.")
+
+            # Accumulate information for the summary
+            summary.append(f"Service: {service_name} - Backup completed successfully")
+        except Exception as e:
+            # Log an error if there is an issue with the backup
+            logging.error(f"Error while backing up service {service_name}: {str(e)}")
+
+            # Accumulate error information for the summary
+            summary.append(f"Service: {service_name} - Error: {str(e)}")
+
+    # Delete folders older than 3 months
+    for folder_info in config["backup_folders"]:
+        service_name = folder_info["service_name"]
+        # Get all directories in the bucket/service_name
+        objects = bucket.list_objects()
+        for obj in objects:
+            obj_key = obj["Key"]
+            if obj_key.startswith(f"{service_name}/"):
+                obj_datetime = obj["LastModified"]
+
+                # Convert obj_datetime to naive datetime for comparison
+                obj_datetime_naive = obj_datetime.replace(tzinfo=None)
+
+                if obj_datetime_naive < current_datetime - timedelta(days=90):
+                    try:
+                        # Delete the folder
+                        bucket.delete_folder(obj_key)
+
+                        # Accumulate information for the summary
+                        summary.append(
+                            f"Deleted folder: {obj_key} - completed successfully."
+                        )
+                    except Exception as e:
+                        # Accumulate information for the summary
+                        summary.append(f"Deleted folder: {obj_key} - Error: {str(e)}")
+
+    # Send a Gotify notification with the summary
+    gotify.create_message(
+        title="Backup Summary",
+        message="\n".join(summary),
+        extras={"format": "text/plain"},
+    )
